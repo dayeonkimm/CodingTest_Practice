39d7402292b0eb72aee863b6b24ae567e127ff65
dayeonkimm <164486991+dayeonkimm@users.noreply.github.com>
2024-06-03 01:38:14 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 0.03 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.py
@@ -0,0 +1,20 @@
+def solution(n):
+    n = str(n)
+    num_list = list(n)
+    new_list = []
+    for i in num_list:
+        k = 0
+        if len(new_list) == 0:
+            new_list.append(i)
+        else:
+            for j in new_list:
+                if i <= new_list[-1]:
+                    new_list.append(i)
+                    break
+                elif i >= j:
+                    new_list.insert(k,i)
+                    break
+                else:
+                    k += 1
+    answer = int("".join(new_list))
+    return answer
