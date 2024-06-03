891668d51d3a1bfca2285ccc40970e9bc902976a
dayeonkimm <164486991+dayeonkimm@users.noreply.github.com>
2024-06-03 20:29:01 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.py
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.py
@@ -1,6 +1,7 @@
 def solution(n):
-    if (n**0.5)%1 == 0:
-        answer = ((n**0.5)+1)**2
+    sqrt = n**0.5
+    if sqrt%1 == 0:
+        answer = (sqrt+1)**2
     else:
         answer = -1
     return answer
