362d83444b28eac706a7be8ac87ae4decf415709
dayeonkimm <164486991+dayeonkimm@users.noreply.github.com>
2024-06-03 22:28:39 +0900

    Update 삼총사.py

--- 프로그래머스/1/131705. 삼총사/삼총사.py
+++ 프로그래머스/1/131705. 삼총사/삼총사.py
@@ -7,3 +7,15 @@ def solution(number):
         if sum(combi) == 0:
             answer += 1
     return answer
+
+
+# def solution(number):
+#     answer = 0
+#     l = len(number)
+#     for i in range(l-2):
+#         for j in range(i+1, l-1):
+#             for k in range(j+1, l):
+#                 # print(number[i],number[j],number[k])
+#                 if number[i]+number[j]+number[k] == 0:
+#                     answer += 1   
+#     return answer
