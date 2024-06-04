201390b4a5de70db256758128ea881506870113d
dayeonkimm <164486991+dayeonkimm@users.noreply.github.com>
2024-06-04 15:30:14 +0900

    Update 행렬의 덧셈.py

--- 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.py
+++ 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.py
@@ -1,3 +1,14 @@
-def solution(A,B):
-    answer = [[c + d for c, d in zip(a,b)] for a, b in zip(A,B)]
+def solution(arr1, arr2):
+    answer = []
+    for a, b in zip(arr1, arr2):
+        row = []
+        for j in range(len(a)):
+            s = a[j] + b[j]
+            row.append(s)
+        answer.append(row)
     return answer
+
+
+# def solution(A,B):
+#     answer = [[c + d for c, d in zip(a,b)] for a, b in zip(A,B)]
+#     return answer
