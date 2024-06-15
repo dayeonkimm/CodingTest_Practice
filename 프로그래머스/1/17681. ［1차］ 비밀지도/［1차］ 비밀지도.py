34273efd7ef33a71636dcceea7edcf35f1fd3d72
dayeonkimm <164486991+dayeonkimm@users.noreply.github.com>
2024-06-16 04:07:29 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.04 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -0,0 +1,40 @@
+def solution(n, arr1, arr2):
+    new_arr1 = []
+    new_arr2 = []
+    for i in arr1:
+        new = ""
+        while i>=2:
+            new += str(i%2)
+            i=i//2
+        new += str(i)
+        new_arr1.append(new[::-1])
+        
+    for j in arr2:
+        new2 = ""
+        while j>=2:
+            new2 += str(j%2)
+            j=j//2
+        new2 += str(j)
+        new_arr2.append(new2[::-1])
+    
+    
+    origin = []
+    for i in range(n):
+        origin.append(list(str(int(new_arr1[i])+int(new_arr2[i]))))
+
+    
+    answer = []
+            
+    for i in origin:
+        answer_part = ""
+        if len(i)<n:
+            for k in range(n-len(i)):
+                answer_part += " "
+        for j in i:
+            if int(j) == 0:
+                answer_part += " "
+            else:
+                answer_part += "#"
+        answer.append(answer_part)
+    
+    return answer
