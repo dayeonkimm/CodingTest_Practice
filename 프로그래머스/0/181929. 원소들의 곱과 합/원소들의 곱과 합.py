a8e2b1e3ff0073750531c67375a7e828c6d41e82
DAYEON <164486991+dayeonkimm@users.noreply.github.com>
2024-06-26 01:24:30 +0900

    [level 0] Title: 원소들의 곱과 합, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.py
@@ -0,0 +1,11 @@
+def solution(num_list):
+    answer1 = 1
+    answer2 = 0
+    for i in num_list:
+        answer1 *= i
+        answer2 += i
+    if answer1 < answer2**2:
+        return 1
+    else:
+        return 0
+    
