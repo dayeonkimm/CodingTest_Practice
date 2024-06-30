a2c003723a8a9a4020d4c91b8a2aa83b0bf28ce0
DAYEON <164486991+dayeonkimm@users.noreply.github.com>
2024-07-01 02:36:47 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 67.64 ms, Memory: 26.2 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,8 +1,7 @@
 def solution(participant, completion):
-    for n in range(len(participant)-len(completion)):
-        completion.append(" ")
-    participant.sort(reverse=True)
-    completion.sort(reverse=True)
-    for i in range(len(participant)):
+    participant.sort()
+    completion.sort()
+    for i in range(len(completion)):
         if participant[i] != completion[i]:
             return participant[i]
+    return participant[-1]
