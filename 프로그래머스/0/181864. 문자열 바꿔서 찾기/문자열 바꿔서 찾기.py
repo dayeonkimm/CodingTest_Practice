9329ba6f7da18d18fc20d8ad60cf11a9d5a430b5
DAYEON <164486991+dayeonkimm@users.noreply.github.com>
2024-07-01 17:58:00 +0900

    [level 0] Title: 문자열 바꿔서 찾기, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.py
@@ -0,0 +1,12 @@
+def solution(myString, pat):
+    new = ""
+    for i in myString:
+        if i=="A":
+            new+="B"
+        else:
+            new+="A"
+    if pat in new:
+        return 1
+    else:
+        return 0
+    
