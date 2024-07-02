1c4913ad6b5844faa508beff2d07d21a57b74f72
DAYEON <164486991+dayeonkimm@users.noreply.github.com>
2024-07-03 01:40:43 +0900

    [level 1] Title: 숫자 짝꿍, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.py
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.py
@@ -1,11 +1,10 @@
 def solution(X, Y):
     answer = ''
-    for i in range(0,10):
+    for i in range(9,-1,-1):
         x=X.count(str(i))
         y=Y.count(str(i))
         answer += min(x,y)*str(i)
-    answer = sorted(list(answer),reverse=True)
-    answer = "".join(answer)
+    
     if answer == "":
         return "-1"
     elif set(answer) == {'0'}:
