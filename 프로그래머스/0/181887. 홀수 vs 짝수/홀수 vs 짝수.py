486083a60a01533fe6f11350f5bab18c7da8827b
DAYEON <ekdyd516@gmail.com>
2024-08-07 23:42:11 +0900

    [level 0] Title: 홀수 vs 짝수, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.py
@@ -0,0 +1,9 @@
+def solution(num_list):
+    answer1 = 0
+    answer2 = 0
+    for i in range(len(num_list)):
+        if i%2 != 0:
+            answer1 += num_list[i]
+        else:
+            answer2 += num_list[i]
+    return max(answer1, answer2)
