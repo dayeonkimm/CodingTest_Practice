d0d35c0c1ae1a0c88c36c8780efe612383b520ca
DAYEON <ekdyd516@gmail.com>
2024-10-03 04:22:10 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.14 ms, Memory: 10.3 MB -BaekjoonHub

--- Python3/프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
+++ Python3/프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
@@ -2,18 +2,7 @@ def solution(survey, choices):
     kdic = {"R":0,"T":0,"C":0,"F":0,"J":0,"M":0,"A":0,"N":0}
     answer = ''
     for i,j in zip(survey,choices):
-        if j==1:
-            kdic[i[0]]+=3
-        elif j==2:
-            kdic[i[0]]+=2
-        elif j==3:
-            kdic[i[0]]+=1
-        elif j==5:
-            kdic[i[1]]+=1
-        elif j==6:
-            kdic[i[1]]+=2
-        elif j==7:
-            kdic[i[1]]+=3
+        kdic[i[0]]+=(4-j)
     if kdic["R"]>=kdic["T"]:
         answer+="R"
     else:
