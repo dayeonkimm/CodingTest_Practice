216747c2a98d5b0ec6fbd851e489e08548cfe8d1
DAYEON <ekdyd516@gmail.com>
2024-12-01 23:41:41 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- Python3/프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.py
+++ Python3/프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.py
@@ -1,24 +1,19 @@
 def solution(keyinput, board):
     a = (board[0]-1)//2
     b = (board[1]-1)//2
-    answer = []
     c = 0
     d = 0
     for i in keyinput:
         if i == "left":
             c -= 1
-            if c < -a:
-                c = -a
+            c = max(c,-a)
         elif i == "right":
             c += 1
-            if c > a:
-                c = a
+            c = min(c,a)
         elif i == "up":
             d += 1
-            if d > b:
-                d = b
+            d = min(d,b)
         else:
             d -= 1
-            if d < -b:
-                d = -b
+            d = max(d, -b)
     return [c,d]
