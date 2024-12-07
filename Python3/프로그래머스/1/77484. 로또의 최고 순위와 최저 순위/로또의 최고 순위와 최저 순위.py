e5920e5371cbff5430f4cd1d55bbd223489289f0
DAYEON <ekdyd516@gmail.com>
2024-12-07 23:57:42 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- Python3/프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.py
+++ Python3/프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.py
@@ -1,20 +1,8 @@
 def solution(lottos, win_nums):
-    answer = []
+    ranks = [6,6,5,4,3,2,1]
     right = 0
     zeros = lottos.count(0)
     for i in lottos:
         if i in win_nums:
             right += 1
-    if right >=2:
-        answer.append(7-right-zeros)
-        answer.append(7-right)
-    elif right == 1:
-        answer.append(6-zeros)
-        answer.append(6)
-    elif right == 0 and zeros == 0:
-        return [6,6]
-    else:
-        answer.append(7-zeros)
-        answer.append(6)
-            
-    return answer
+    return [ranks[right+zeros],ranks[right]]
